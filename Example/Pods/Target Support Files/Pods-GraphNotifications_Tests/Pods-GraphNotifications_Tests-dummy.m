b43fd57c9bfcb0ac6249b89e918a788b0928f1a9
Adam <venne1138@gmail.com>
2019-08-13 09:47:53 -0700

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-GraphNotifications_Tests/Pods-GraphNotifications_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GraphNotifications_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GraphNotifications_Tests
+@end
---
+++ GraphNotifications/Classes/GNAuthenticiation.h
@@ -0,0 +1,12 @@
+//
+//  GNAuthenticiation.h
+//  GraphNotifications
+//
+//  Created by Adam Crabtree on 8/13/19.
+//
+
+#ifndef GNAuthenticiation_h
+#define GNAuthenticiation_h
+
+
+#endif /* GNAuthenticiation_h */
